bb18852a28220bb9a31051e8d2ee03a6a00688f9
DELL <maryamnzlawy@gmail>
2024-10-01 08:42:27 +0300

    Add "Set Driver Type" function in the base test class to switch the driver type to help in the cross-browser test

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,8 +2,13 @@ package base;
 
 import com.gobus.pages.base.BasePage;
 import com.gobus.pages.base.HomePage;
+import org.openqa.selenium.HasAuthentication;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -17,10 +22,23 @@ public class BaseTest {
     protected HomePage homePage;
     private String GoBus_URL = "https://go-bus.com/?lang=en";
 
+
+
+    public WebDriver setDriverType(String driverType){
+        return switch (driverType) {
+            case "chrome" -> new ChromeDriver();
+            case "edge" -> new EdgeDriver();
+            case "firefox" -> new FirefoxDriver();
+            case "safari" -> new SafariDriver();
+            case "internet explorer" -> new InternetExplorerDriver();
+            default -> throw new IllegalArgumentException("Invalid driver type: " + driverType);
+        };
+    }
+
     @BeforeMethod
     public void setUp() {
         // Initialize the WebDriver
-        driver = new ChromeDriver();
+        driver = setDriverType("edge");
         driver.manage().window().maximize();
 
         // Navigate to the GoBus URL
